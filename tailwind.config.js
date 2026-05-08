c01b486e6f95b67558aca891906fc6e7cc3c3f83
Thamires <seu-email-do-github@email.com>
2026-05-08 18:56:13 -0300

    corrige tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: '#fdf2f8',
-          100: '#fce7f3',
-          200: '#fbcfe8',
-          300: '#f9a8d4',
-          400: '#f472b6',
-          500: '#ec4899',
-          600: '#db2777',
-          700: '#be185d',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
