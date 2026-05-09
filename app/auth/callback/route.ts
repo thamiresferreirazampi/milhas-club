a31e543935c544b5e2b89acd2e93ec5fd027e3dd
Thamires <seu-email-do-github@email.com>
2026-05-09 18:00:27 -0300

    auth callback

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js'
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type')
+
+  if (token_hash && type) {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+
+    await supabase.auth.verifyOtp({ token_hash, type: type as any })
+  }
+
+  return NextResponse.redirect(new URL('/login', request.url))
+}
